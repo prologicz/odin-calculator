802a7eb8bf67b47fd87f3402ec76cc05c75223ca
Mecha Z <czbills88@gmail.com>
2022-09-20 20:47:01 -0400

    Capture user input of values

--- script.js
+++ script.js
@@ -23,3 +23,25 @@ function operate (num1, operator, num2) {
     if (operator == '/') return divide(num1, num2)
 }
 
+buttonArea = document.querySelector('.calculator-buttons')
+displayArea = document.querySelector('.display')
+numberButtons = buttonArea.querySelectorAll('.number-button')
+calculationButtons = buttonArea.querySelectorAll('.calculation-button')
+equalsButton = buttonArea.querySelector('.equals-button')
+clearButton = buttonArea.querySelector('.clear-button')
+
+
+let displayArray = []
+let inputValue
+numberButtons.forEach(button => button.addEventListener('click', (e) =>{
+
+    pressedButton = e.srcElement.innerText
+    numberButtonPress = document.createElement('div')
+    numberButtonPress.id = 'number-display'
+    numberButtonPress.textContent = e.srcElement.innerText;    
+    displayArea.appendChild(numberButtonPress)
+    displayArray.push(pressedButton)
+    inputValue = parseInt(displayArray.join(''))
+    console.log(inputValue)
+}))
+
